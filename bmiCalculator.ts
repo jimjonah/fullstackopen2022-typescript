8e3eeb7c131576a491de1adc01c632c8f30b51ab
jjonah <jjonah@veracode.com>
2023-03-18 15:31:21 -0400

    9.3 Command line

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,11 +1,30 @@
 //Body Mass Index (BMI) is a person's weight in kilograms (or pounds) divided by the square of height in meters (or feet).
 
 
+interface CalcNumbers {
+  height: number;
+  weight: number;
+}
+
+const parseArguments = (args: string[]): CalcNumbers => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3])
+    }
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+}
+
 function calculateBmi(centimeters: number, kilograms: number): string {
   let meter = centimeters / 100;
 
   let bmi = kilograms / (meter * meter);
-  console.log('bmi', bmi);
+  // console.log('bmi', bmi);
 
   if (bmi < 18.5) {
     return ('Underweight');
@@ -18,8 +37,26 @@ function calculateBmi(centimeters: number, kilograms: number): string {
   }
 }
 
-console.log(calculateBmi(180, 55));
-console.log(calculateBmi(180, 74));
-console.log(calculateBmi(180, 84));
-console.log(calculateBmi(180, 100));
+// console.log(calculateBmi(180, 55));
+// console.log(calculateBmi(180, 74));
+// console.log(calculateBmi(180, 84));
+// console.log(calculateBmi(180, 100));
 
+try {
+  const { height, weight } = parseArguments(process.argv);
+  console.log(calculateBmi(height, weight));
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.'
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
+
+// npm run calculateBmi 180 100
+// npm run calculateBmi 180 84
+// npm run calculateBmi 180 74
+// npm run calculateBmi 180 55
+
+
+export default calculateBmi;
--- exerciseCalculator.ts
+++ exerciseCalculator.ts
@@ -1,8 +1,30 @@
+
 interface TrainingValues {
   trainingData: Array<number>;
   targetValue: number;
 }
 
+
+const parseArguments = (args: string[]): TrainingValues => {
+  if (args.length < 3) throw new Error('Not enough arguments');
+
+  let trainingData = [];
+  for (let i = 2; i < args.length; i++) {
+    // console.log('args[i]', args[i]);
+    // console.log('!isNaN(Number(args[i])', !isNaN(Number(args[i])));
+
+    if (!isNaN(Number(args[i]))) {
+      trainingData.push(Number(args[i]));
+    } else {
+      throw new Error('Provided values were not numbers!');
+    }
+  }
+
+  return {
+    trainingData: trainingData,
+    targetValue: trainingData[trainingData.length - 1]
+  }
+}
 
 interface TrainingResult {
   periodLength: number,
@@ -15,16 +37,16 @@ interface TrainingResult {
 }
 
 
-const calculateExercises = (trainingData: Array<number>, targetHours:number): TrainingResult => {
+const calculateExercises = (trainingData: Array<number>, targetHours: number): TrainingResult => {
   let trainingDays = 0;
   let averageHours = 0;
   let totalHours = 0;
 
   trainingData.forEach((val) => {
-    if(val !== 0){
+    if (val !== 0) {
       trainingDays++;
       totalHours += val;
-      averageHours = totalHours/trainingDays;
+      averageHours = totalHours / trainingDays;
     }
   });
 
@@ -32,16 +54,16 @@ const calculateExercises = (trainingData: Array<number>, targetHours:number): Tr
   // if exercise days median days but average hours is slightly below target then 2
   // otherwise 1
 
-  let averageExerciseDays = trainingDays / trainingData.length ;
+  let averageExerciseDays = trainingDays / trainingData.length;
   console.log('averageExerciseDays', averageExerciseDays);
   let rating = 1;
   let message = 'Let\'s step it up a bit!';
 
-  if(averageExerciseDays >= .5 && averageHours > targetHours){
+  if (averageExerciseDays >= .5 && averageHours > targetHours) {
     rating = 3;
     message = 'Awesome! Keep it up!';
-  }else if(averageExerciseDays >= .5 && averageHours < targetHours ||
-    averageExerciseDays < .5 && averageHours > targetHours){
+  } else if (averageExerciseDays >= .5 && averageHours < targetHours ||
+    averageExerciseDays < .5 && averageHours > targetHours) {
     rating = 2;
     message = 'not too bad but could be better';
   }
@@ -57,8 +79,26 @@ const calculateExercises = (trainingData: Array<number>, targetHours:number): Tr
   };
 }
 
+//
+// console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
+// console.log(calculateExercises([1.5, 0, 2, 2.1, 0, 1.4, 1], 2));
+// console.log(calculateExercises([4.5, 0, 0, 5.1, 0, 1.4, 0], 2));
+// console.log(calculateExercises([1.5, 0, 0, 2.1, 0, 1.4, 0], 2));
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
-console.log(calculateExercises([1.5, 0, 2, 2.1, 0, 1.4, 1], 2));
-console.log(calculateExercises([4.5, 0, 0, 5.1, 0, 1.4, 0], 2));
-console.log(calculateExercises([1.5, 0, 0, 2.1, 0, 1.4, 0], 2));
+try {
+  const { trainingData, targetValue } = parseArguments(process.argv);
+  console.log(calculateExercises(trainingData, targetValue));
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.'
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
+
+// npm run calculateExercises 3 0 2 4.5 0 3 1 2
+// npm run calculateExercises 1.5 0 2 2.1 0 1.4 1 2
+// npm run calculateExercises 4.5 0 0 5.1 0 1.4 0 2
+// npm run calculateExercises 1.5 0 0 2.1 0 1.4 0 0 2
+
+export default calculateExercises;
