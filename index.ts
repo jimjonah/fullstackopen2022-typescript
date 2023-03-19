f75853994b0cd7815c85f89fbc943daf748e68c1
jjonah <jjonah@veracode.com>
2023-03-19 09:09:29 -0400

    9.4 Express

---
+++ index.ts
@@ -0,0 +1,19 @@
+import {Express} from 'express';
+import { Request, Response } from 'express'
+const express = require('express');
+
+const app:Express = express();
+
+app.get('/ping', (_req: Request, res: Response) => {
+  res.send('pong');
+});
+
+app.get('/hello', (_req: Request, res: Response) => {
+  res.send('Hello Full Stack!');
+});
+
+const PORT = 3003;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
