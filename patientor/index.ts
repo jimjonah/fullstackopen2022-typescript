9e543135c11ea2b1b120fcd37d735e4a650ffd07
jjonah <jjonah@veracode.com>
2023-03-20 07:40:28 -0400

    9.9: Patientor backend, step2

--- patientor/index.ts
+++ patientor/index.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 const app = express();
 app.use(express.json());
+const cors = require('cors')
 
-const PORT = 3000;
+app.use(cors())
+
+const PORT = 3001;
 
 app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
