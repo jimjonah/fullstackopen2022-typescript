1859e1bb4e79b0b047dd10e0ae0b1ccd6be3257a
jjonah <jjonah@veracode.com>
2023-03-21 11:41:31 -0400

    9.14 - react - typescript starter project

---
+++ react_ts/react-ts-jj/src/App.tsx
@@ -0,0 +1,31 @@
+import Header from "./Header";
+import Content from "./Content";
+import Footer from "./Footer";
+
+const App = () => {
+  const courseName = "Half Stack application development";
+  const courseParts = [
+    {
+      name: "Fundamentals",
+      exerciseCount: 10
+    },
+    {
+      name: "Using props to pass data",
+      exerciseCount: 7
+    },
+    {
+      name: "Deeper type usage",
+      exerciseCount: 14
+    }
+  ];
+
+  return (
+    <div>
+      <Header courseName={courseName} />
+      <Content courseParts={courseParts} />
+      <Footer courseParts={courseParts} />
+    </div>
+  );
+};
+
+export default App;
